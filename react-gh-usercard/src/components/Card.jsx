2da6262b89c128e8b8ad5e025367fafbdce74925
Megan <mennis0145@gmail.com>
2019-10-01 12:20:00 +0100

    Component structure loosely scaffolded

---
+++ react-gh-usercard/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class Card extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>UserCard</div> 
+        );
+    } 
+}
